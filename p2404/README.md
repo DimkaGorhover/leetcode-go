3928924ec7e81da330a445c415144a98f4159d77
Dmytro Horkhover <gd.mail.89@gmail.com>
2022-09-19 23:55:27 +0300

    LeetCode #2404: Most Frequent Even Element

---
+++ p2404/README.md
@@ -0,0 +1,1 @@
+# [LeetCode #2404: Most Frequent Even Element](https://leetcode.com/problems/most-frequent-even-element/)
---
+++ p2404/solution_test.go
@@ -0,0 +1,29 @@
+package p2404
+
+import "testing"
+
+func Test_mostFrequentEven(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: ``,
+			args: args{
+				nums: []int{0, 1, 2, 2, 4, 4, 1},
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := mostFrequentEven(tt.args.nums); got != tt.want {
+				t.Errorf("mostFrequentEven() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
