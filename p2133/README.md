40443b9e22a9cf55f53592f6503f360e46c9addb
Dmytro Horkhover <gd.mail.89@gmail.com>
2022-09-26 15:51:09 +0300

    LeetCode #2133: Check if Every Row and Column Contains All Numbers

---
+++ p2133/README.md
@@ -0,0 +1,1 @@
+# [LeetCode #2133: Check if Every Row and Column Contains All Numbers](https://leetcode.com/problems/check-if-every-row-and-column-contains-all-numbers/)
---
+++ p2133/solution_test.go
@@ -0,0 +1,44 @@
+package p2133
+
+import "testing"
+
+func Test_checkValid(t *testing.T) {
+	type args struct {
+		matrix [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: ``,
+			args: args{
+				matrix: [][]int{
+					{1, 2, 3},
+					{3, 1, 2},
+					{2, 3, 1},
+				},
+			},
+			want: true,
+		},
+		{
+			name: ``,
+			args: args{
+				matrix: [][]int{
+					{1, 1, 1},
+					{1, 2, 3},
+					{1, 2, 3},
+				},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := checkValid(tt.args.matrix); got != tt.want {
+				t.Errorf("checkValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
