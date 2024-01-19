5a20f7634397700e95cc276db4d24167aaaf5435
Dmytro Horkhover <gd.mail.89@gmail.com>
2024-01-19 21:49:53 +0200

    LeetCode #344: Reverse String

---
+++ p0344/README.md
@@ -0,0 +1,1 @@
+# [LeetCode #344: Reverse String](https://leetcode.com/problems/reverse-string/)
---
+++ p0344/solution_test.go
@@ -0,0 +1,32 @@
+package p0344
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_reverseString(t *testing.T) {
+	t.Parallel()
+	type args struct {
+		s []byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want []byte
+	}{
+		{
+			name: `test_001`,
+			args: args{
+				s: []byte("hello world"),
+			},
+			want: []byte("dlrow olleh"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			reverseString(tt.args.s)
+			assert.Equal(t, tt.want, tt.args.s)
+		})
+	}
+}
